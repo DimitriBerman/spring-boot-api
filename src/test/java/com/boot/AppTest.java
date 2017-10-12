2056d46aa03f03cb5aae8d366c22b7c18c017645
Nicolas Len <nlen@virtualmind.io>
2017-10-12 19:13:04 -0300

    added test

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,38 +1,19 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.boot.controller.HomeController;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.Assert.assertEquals;
+
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    @Test
+    public void ApptestTest( )
     {
-        super( testName );
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals(result,"Das boot, reporting for duty!");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
